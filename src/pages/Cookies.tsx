f8afe42ef46d4d21f6b3b8eb004d30ebc2e64da7
Yuri Volkov <0@mcornholio.ru>
2025-12-01 16:13:10 +0900

    test: fix e2e-tests tsconfig

--- src/pages/Cookies.tsx
+++
@@ -1,4 +0,0 @@
-import { PdfViewer } from '../components/pdfViewer/PdfViewer';
-import cookies from './../assets/documents/DATA_RIVER_COOKIES.pdf';
-
-export const Cookies = () => <PdfViewer file={cookies} />;
--- src/pages/TermsAndConditions.tsx
+++
@@ -1,4 +0,0 @@
-import { PdfViewer } from '../components/pdfViewer/PdfViewer';
-import terms from './../assets/documents/DATA_RIVER_TERMS_AND_CONDITIONS.pdf';
-
-export const TermsAndConditions = () => <PdfViewer file={terms} />;
